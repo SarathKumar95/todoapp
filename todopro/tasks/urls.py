c1c417b293bce7ab85a9ca41d72f4930e8e88c60
Sarath Kumar <worksarath95@gmail.comgit config --global user.email worksarath95@gmail.comgit config --global user.email worksarath95@gmail.com>
2022-02-25 17:51:21 +0530

    added delete template and view for item deletion

--- todopro/tasks/urls.py
+++ todopro/tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from tasks import views
 
 urlpatterns = [
-    path('',views.index)
+    path('', views.index, name='home'),
+    path('delete/<str:pk>/', views.delete_task, name='delete'),
 ]
--- todopro/tasks/views.py
+++ todopro/tasks/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tasks.models import *
 from tasks.forms import *
+
+
 # Create your views here.
 
 def index(request):
@@ -14,11 +16,16 @@ def index(request):
             form.save()
         return redirect('/')
 
-    context = {"tasks": tasks, "form":form}
+    context = {"tasks": tasks, "form": form}
 
     return render(request, 'home.html', context)
 
 
+def delete_task(request, pk):
+    item = Tasks.objects.get(id=pk)
 
-
-
+    if request.method == "POST":
+        item.delete()
+        return redirect('/')
+    context = {"item": item}
+    return render(request, 'delete.html',context )
