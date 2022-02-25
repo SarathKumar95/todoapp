5c2901f70ce9fbfdb0893015ee4263f3508b6ee9
Sarath Kumar <worksarath95@gmail.comgit config --global user.email worksarath95@gmail.comgit config --global user.email worksarath95@gmail.com>
2022-02-25 17:23:01 +0530

    created forms.py and rendered html with post req

--- todopro/tasks/views.py
+++ todopro/tasks/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from tasks.models import *
+from tasks.forms import *
 # Create your views here.
 
 def index(request):
     tasks = Tasks.objects.all()
-    context = {"tasks": tasks}
+    form = TaskForm()
+
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {"tasks": tasks, "form":form}
 
     return render(request, 'home.html', context)
 
